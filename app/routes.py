7d7b397dbd1b5e422ec389f329b4e48e6179d343
Laura Skomerza <lskomerza@gmail.com>
2022-10-21 11:14:59 -0700

    updated Planet class and get all planets list

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+planets = [
+    Planet(1, "Mars", "planet closest to the sun", ["Phobos", "Deimos"]), 
+    Planet(2, "Venus", "hottest planet", "None"),
+    Planet(3, "Earth", "planet with people", "moon")
+]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planet")
+
+@planets_bp.route("", methods=["GET"])
+
+def get_all_planets():
+    response = []
+    for planet in planets:
+        this_one = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moons": planet.moons
+        }
+        response.append(this_one)
+    return jsonify(response), 200
