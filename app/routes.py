6d9c3eda3f968e8319052babcbf992104ed0b934
Laura Skomerza <lskomerza@gmail.com>
2022-11-01 11:39:54 -0700

    deleted old code/comments and added query param for name

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,7 @@
 from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.planet import Planets
-'''
-class Planet:
-    def __init__(self, id, name, description, moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moons = moons
 
-planets = [
-    Planet(1, "Mars", "planet closest to the sun", ["Phobos", "Deimos"]), 
-    Planet(2, "Venus", "hottest planet", "None"),
-    Planet(3, "Earth", "planet with people", "moon")
-]
-'''
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planet")
 
 def get_one_planet_or_abort(planet_id):
@@ -51,7 +38,14 @@ def add_planet():
 @planets_bp.route("", methods=["GET"])
 
 def get_all_planets():
-    planets = Planets.query.all()
+    name_param = request.args.get("name")
+
+    if name_param is None:
+        planets = Planets.query.all()
+    else:
+        planets = Planets.query.filter_by(name=name_param)
+        
+
     response = []
     for planet in planets:
         this_one = {
@@ -102,26 +96,5 @@ def delete_one_planet(planets_id):
 
     return jsonify({"message": f"Successfully deleted planet with id {planets_id}"}), 200
 
-'''
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        response_message = f"Error. {planet_id} must be an integer."
-        return jsonify({"Message": response_message}), 400
 
-    for planet in planets:
-        if planet.id == planet_id:
-            this_one = {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "moons": planet.moons
-            }
-            return jsonify(this_one), 200
-    
-    response_404 = f"No planet with this id {planet_id}"
-    return jsonify({"Message": response_404}), 404
-    '''
 
